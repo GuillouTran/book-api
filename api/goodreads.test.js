79e08175087a7fb41b5a71f0bc6697aa7ad18fc7
Will Guillou <thnghia6063@gmail.com>
2020-12-19 14:27:22 +0000

    att

--- api/goodreads.test.js
+++
@@ -1,9 +0,0 @@
-const GoodReads = require("./goodreads");
-const Context = require("../").Context;
-const test = require("tap").test;
-
-test("GoodReads", async t => {
-  const ctx = new Context();
-  const gr = new GoodReads(ctx);
-  t.matchSnapshot(await gr.ISBN("0812993543"));
-});
--- guess.test.js
+++
@@ -1,8 +0,0 @@
-const test = require("tap").test;
-const guess = require("./guess");
-
-test("simple cases", t => {
-  t.equal(guess("OL44998W"), "OLID");
-  t.equal(guess("9780062368751"), "ISBN");
-  t.equal(guess("0349141924"), "ISBN");
-});
